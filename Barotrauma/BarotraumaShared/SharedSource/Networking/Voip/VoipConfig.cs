69f1f17c9a07f5c4102eb68ecc196cb799f79b62
mygamingaccount <68995233+mygamingaccount@users.noreply.github.com>
2024-11-20 00:02:19 +0100

    High definition voice config

--- Barotrauma/BarotraumaShared/SharedSource/Networking/Voip/VoipConfig.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Networking/Voip/VoipConfig.cs
@@ -7,12 +7,12 @@ namespace Barotrauma.Networking
 {
     static partial class VoipConfig
     {
-        public const int MAX_COMPRESSED_SIZE = 40; //amount of bytes we expect each 20ms of audio to fit in
+        public const int MAX_COMPRESSED_SIZE = 80; //amount of bytes we expect each 20ms of audio to fit in
 
         public static readonly TimeSpan SEND_INTERVAL = new TimeSpan(0,0,0,0,20);
 
         public const int FREQUENCY = 48000; //48Khz
-        public const int BITRATE = 16000; //16Kbps
+        public const int BITRATE = 32000; //16Kbps
         public const int BUFFER_SIZE = (8 * MAX_COMPRESSED_SIZE * FREQUENCY) / BITRATE; //20ms window
 
         public static OpusDecoder CreateDecoder()
